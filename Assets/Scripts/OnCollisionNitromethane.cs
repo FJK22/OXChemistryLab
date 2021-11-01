e2a8c3cc4748f2329598f80edb4eca00eb58c90a
Fjorda Kazazi <79405767+FJK22@users.noreply.github.com>
2021-11-01 14:33:51 +0000

    update exam3

---
+++ Assets/Scripts/OnCollisionNitromethane.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnitySimpleLiquid;
+using Valve.VR.InteractionSystem;
+using Valve.VR.InteractionSystem.Sample;
+
+public class OnCollisionNitromethane : MonoBehaviour
+{
+    public GameObject FluidPlate;
+
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.gameObject.tag == "FluidPlate")
+        {
+
+            FluidPlate.GetComponent<LiquidContainer>().fillAmountPercent = 0.3f;
+         
+
+
+        }
+    }
+}
